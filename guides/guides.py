7ce92ad658e6c3f2eefcb37e6d6d89a34d0c33c7
TJ Phillips <tjphillipsii@gmail.com>
2018-02-13 22:50:46 -0700

    Fixing the Help.. maybe, again?

--- guides/guides.py
+++ guides/guides.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+from __main__ import send_cmd_help
 
 
 class Guides:
@@ -20,31 +21,31 @@ class Guides:
             await self.bot.send_cmd_help(ctx)
 
     @_guides.command(pass_context=True, no_pm=True)
-    async def calus(self):
+    async def calus(self, ctx):
         """Calus Guide"""
         await self.bot.say('Here is the guide for Calus:')
         await self.bot.say('https://youtu.be/aeKeJ6x_Zi8')
 
     @_guides.command(pass_context=True, no_pm=True)
-    async def gauntlet(self):
+    async def gauntlet(self, ctx):
         """Gauntlent Guide"""
         await self.bot.say('Here is the guide for the Gauntlet:')
         await self.bot.say('https://youtu.be/T0DbeXjkKRs')
 
     @_guides.command(pass_context=True, no_pm=True)
-    async def baths(self):
+    async def baths(self, ctx):
         """Guide for the Baths and Enterance"""
         await self.bot.say('Here is the guide for the Enterancee and Baths:')
         await self.bot.say('https://youtu.be/FVAvumqd2Yw')
 
     @_guides.command(pass_context=True, no_pm=True)
-    async def gardens(self):
+    async def gardens(self, ctx):
         """Guide for the Pleasure Gardens"""
         await self.bot.say('Here is the guide for the Pleasure Gardens:')
         await self.bot.say('https://youtu.be/pPg7rxKxf8o')
 
     @_guides.command(pass_context=True, no_pm=True)
-    async def spawns(self):
+    async def spawns(self, ctx):
         """Some good info on spawns in the Throneroom"""
         await self.bot.say('Spawn pattern of ads in throne room:')
         await self.bot.say('https://redd.it/79gpxc')
