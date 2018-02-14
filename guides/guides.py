9080126aae47803a450eedb95a836ce552f05f6f
TJ Phillips <tjphillipsii@gmail.com>
2018-02-13 22:17:48 -0700

    New Cog for guides

---
+++ guides/guides.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+import os
+
+
+class Guides:
+
+    """Guides
+
+    Guide Videos Provided by ShadowsGirl4Life"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.group(name="guides", pass_context=True)
+    async def _guides(self, ctx):
+        """You must enter a guide from the list"""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @_guides.command(pass_context=True, no_pm=True)
+    async def calus(self):
+        """Calus Guide"""
+        await self.bot.say('Here is the guide for Calus:')
+        await self.bot.say('https://youtu.be/aeKeJ6x_Zi8')
+
+    @_guides.command(pass_context=True, no_pm=True)
+    async def gauntlet(self):
+        """Gauntlent Guide"""
+        await self.bot.say('Here is the guide for the Gauntlet:')
+        await self.bot.say('https://youtu.be/T0DbeXjkKRs')
+
+    @_guides.command(pass_context=True, no_pm=True)
+    async def baths(self):
+        """Guide for the Baths and Enterance"""
+        await self.bot.say('Here is the guide for the Enterancee and Baths:')
+        await self.bot.say('https://youtu.be/FVAvumqd2Yw')
+
+    @_guides.command(pass_context=True, no_pm=True)
+    async def gardens(self):
+        """Guide for the Pleasure Gardens"""
+        await self.bot.say('Here is the guide for the Pleasure Gardens:')
+        await self.bot.say('https://youtu.be/pPg7rxKxf8o')
+
+    @_guides.command(pass_context=True, no_pm=True)
+    async def spawns(self):
+        """Some good info on spawns in the Throneroom"""
+        await self.bot.say('Spawn pattern of ads in throne room:')
+        await self.bot.say('https://redd.it/79gpxc')
+
+
+def setup(bot):
+    n = Guides(bot)
+    bot.add_cog(n)
